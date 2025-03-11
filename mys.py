0b3e6f387c4e63179574e3e331ae778a116612f4
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 12:07:43 +0000

    Update query in mys.py to fetch users with names containing 'at' and age greater than 30

--- mys.py
+++ mys.py
@@ -52,9 +52,18 @@ my_curser = cnx.cursor()
 
 
 # WHERE LIKE and Wildcards
-my_curser.execute('SELECT * FROM users WHERE name LIKE "%at%"')
+# my_curser.execute('SELECT * FROM users WHERE name LIKE "%at%"')
+# print('NAME\t\tEMAIL\t\t\t\tAGE\t\tID')
+# print('----\t\t-----\t\t\t\t---\t\t---')
+# result = my_curser.fetchall()
+# for row in result:
+#         print(row[0], '\t\t%s' %row[1], '\t\t%s' %row[2], '\t\t%s' %row[3])
+
+# AND / OR clause
+my_curser.execute('SELECT * FROM users WHERE name LIKE "%at%" AND age > 30')
 print('NAME\t\tEMAIL\t\t\t\tAGE\t\tID')
 print('----\t\t-----\t\t\t\t---\t\t---')
 result = my_curser.fetchall()
 for row in result:
         print(row[0], '\t\t%s' %row[1], '\t\t%s' %row[2], '\t\t%s' %row[3])
+
