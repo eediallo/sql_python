ff556b59c9bf742197891816e4788d9766a5829d
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 14:19:35 +0000

    Add functionality to query and display student records from the database

--- p_sqlite/sq.py
+++ p_sqlite/sq.py
@@ -8,24 +8,34 @@ curr = conn.cursor()
 # curr.execute('CREATE TABLE Students(roll INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, city TEXT, deptno INTEGER, FOREIGN KEY(deptno) REFERENCES Dept(deptno))')
 
 # INSERT DATA
-query = ('INSERT INTO Students (name, city) VALUES (?, ?)')
+# query = ('INSERT INTO Students (name, city) VALUES (?, ?)')
 
-students = [
-    ('Elhadj', 'London'),
-    ('Kadi', 'Montreal'),
-    ('Fatim', 'Conakry'),
-    ('Mariam', 'Dakar'),
-    ('Ousmane', 'Paris'),
-    ('Alpha', 'Accra'),
-    ('Fatou', 'Birminghan'),
-    ('Ibrahim', 'Mamou'),
-    ('Abdoul', 'London'),
-    ('Binta', 'Marseille'),
-]
+# students = [
+#     ('Elhadj', 'London'),
+#     ('Kadi', 'Montreal'),
+#     ('Fatim', 'Conakry'),
+#     ('Mariam', 'Dakar'),
+#     ('Ousmane', 'Paris'),
+#     ('Alpha', 'Accra'),
+#     ('Fatou', 'Birminghan'),
+#     ('Ibrahim', 'Mamou'),
+#     ('Abdoul', 'London'),
+#     ('Binta', 'Marseille'),
+# ]
 
-curr.executemany(query, students)
+# curr.executemany(query, students)
 
-conn.commit()
+# QUERY DATA
+query = 'SELECT * FROM Students'
+curr.execute(query)
+result = curr.fetchall()
+print('============================================')
+print('ID \tNAME\t\tCity')
+print('--- \t----\t\t----')
+for student in result:
+    print(student[0], '\t%s' %student[1], '\t\t%s' %student[2])
+print('============================================')
+# conn.commit()
 
 curr.close()
 conn.close()
