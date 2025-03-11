a7971c8fb80368e1dc12a75859c23cb23c4d6706
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 15:05:01 +0000

    Insert multiple products into the Product table in shop.py

--- p_sqlite/shop.py
+++ p_sqlite/shop.py
@@ -5,8 +5,24 @@ conn = sqlite3.connect('shop.db')
 curr = conn.cursor()
 
 
-# Create tables
-query = 'CREATE TABLE Orders(OrderNo TEXT PRIMARY KEY, Custid TEXT, ProdNo TEXT, Qty INTEGER, FOREIGN KEY(Custid) REFERENCES Customer(Custid), FOREIGN KEY(ProdNo) REFERENCES Product(ProdNo))'
-curr.execute(query)
+# # Create tables
+# query = 'CREATE TABLE Orders(OrderNo TEXT PRIMARY KEY, Custid TEXT, ProdNo TEXT, Qty INTEGER, FOREIGN KEY(Custid) REFERENCES Customer(Custid), FOREIGN KEY(ProdNo) REFERENCES Product(ProdNo))'
+# curr.execute(query)
+
+# INSERT INTO TABLE
+query = 'INSERT INTO Product(ProdNo, Pname, Price)VALUES(?, ?, ?)'
+
+products = [
+    ('P10', 'Haj', 'London Bridge'),
+    ('P11', 'Fatma', 'Uxbrigde, Denham'),
+    ('P12', 'Donara', 'Mali Yembery'),
+    ('P13', 'Naika', 'Ditinn, Fatako'),
+    ('P14', 'Rihana', 'Paris, France'),
+    ('P15', 'Daniel', 'Madrid, Spain'),
+]
+
+curr.executemany(query, products)
+conn.commit()
+
 curr.close()
 conn.close()
