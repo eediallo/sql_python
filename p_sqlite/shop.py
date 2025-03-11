941f9c8d3617eb35cc2ba543aaa9e29161c9bc59
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 15:16:42 +0000

    Update product entries in shop.py with new names and prices

--- p_sqlite/shop.py
+++ p_sqlite/shop.py
@@ -9,16 +9,23 @@ curr = conn.cursor()
 # query = 'CREATE TABLE Orders(OrderNo TEXT PRIMARY KEY, Custid TEXT, ProdNo TEXT, Qty INTEGER, FOREIGN KEY(Custid) REFERENCES Customer(Custid), FOREIGN KEY(ProdNo) REFERENCES Product(ProdNo))'
 # curr.execute(query)
 
+
+# createQuery = 'CREATE TABLE Product(ProdNo TEXT PRIMARY Key, Pname TEXT, Price FLOAT)'
+# curr.execute(createQuery)
+
 # INSERT INTO TABLE
+# dropQuery = 'DROP TABLE Product'
+# curr.execute(dropQuery)
+
 query = 'INSERT INTO Product(ProdNo, Pname, Price)VALUES(?, ?, ?)'
 
 products = [
-    ('P10', 'Haj', 'London Bridge'),
-    ('P11', 'Fatma', 'Uxbrigde, Denham'),
-    ('P12', 'Donara', 'Mali Yembery'),
-    ('P13', 'Naika', 'Ditinn, Fatako'),
-    ('P14', 'Rihana', 'Paris, France'),
-    ('P15', 'Daniel', 'Madrid, Spain'),
+    ('P10', 'Toothpaste', 20.8),
+    ('P11', 'ToothBrush', 10.8),
+    ('P12', 'Champoo', 88.9),
+    ('P13', 'Milk', 9.6),
+    ('P14', 'Soap', 5.8),
+    ('P15', 'Farime', 34.8),
 ]
 
 curr.executemany(query, products)
