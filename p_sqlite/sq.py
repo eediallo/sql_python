ca8d9d2a241af138d2d79b3fe685adefd8019107
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 13:42:21 +0000

    Insert initial department records into the database

--- p_sqlite/sq.py
+++ p_sqlite/sq.py
@@ -2,3 +2,23 @@ import sqlite3
 
 
 conn = sqlite3.connect('forum.db')
+
+curr = conn.cursor()
+
+# curr.execute('CREATE TABLE Students(roll INTEGER PRIMARY Key AUTO_INCREMENT, name TEXT,city TEXT, deptno INTEGER, FOREIGN Key(deptno) references Dept(deptno))')
+
+# INSERT DATA
+
+query = 'INSERT INTO Dept(name) VALUES(?)'
+students = [
+    ('Economics',),
+    ('Science',),
+    ('Biology',),
+    ('IT',),
+]
+
+curr.executemany(query, students)
+conn.commit()
+
+curr.close()
+conn.close()
