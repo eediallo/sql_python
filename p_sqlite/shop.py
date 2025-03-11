a04b48ed54a69786643c377cf29b9350cbc11985
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 15:40:31 +0000

    Retrieve and print customer data from the database in shop.py

--- p_sqlite/shop.py
+++ p_sqlite/shop.py
@@ -17,21 +17,25 @@ curr = conn.cursor()
 # dropQuery = 'DROP TABLE Product'
 # curr.execute(dropQuery)
 
-query = 'INSERT INTO Orders(OrderNo, Custid, ProdNo, Qty)VALUES(?, ?, ?, ?)'
+# query = 'INSERT INTO Orders(OrderNo, Custid, ProdNo, Qty)VALUES(?, ?, ?, ?)'
 
-oders = [
-    (10005, 'P002', 'P15', 8),
-    (10003, 'P001', 'P12', 5),
-    (10006, 'P006', 'P10', 80),
-    (10001, 'P003', 'P11', 30),
-    (10111, 'P004', 'P14', 8),
-    (10020, 'P001', 'P11', 9),
-    (10021, 'P002', 'P10', 29),
-    (11111, 'P002', 'P15', 189),
-]
+# oders = [
+#     (10005, 'P002', 'P15', 8),
+#     (10003, 'P001', 'P12', 5),
+#     (10006, 'P006', 'P10', 80),
+#     (10001, 'P003', 'P11', 30),
+#     (10111, 'P004', 'P14', 8),
+#     (10020, 'P001', 'P11', 9),
+#     (10021, 'P002', 'P10', 29),
+#     (11111, 'P002', 'P15', 189),
+# ]
 
-curr.executemany(query, oders)
-conn.commit()
-
+# curr.executemany(query, oders)
+# conn.commit()
+cQuery = ('SELECT * FROM Customer')
+curr.execute(cQuery)
+result = curr.fetchall()
+for customer in result:
+    print(customer)
 curr.close()
 conn.close()
