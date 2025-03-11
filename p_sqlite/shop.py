c3b743b009bc952e0b9acc08359d944e22046bf6
eediallo <elhhabdouldiallo@gmail.com>
2025-03-11 14:57:02 +0000

    Add Orders table creation to shop.py

--- p_sqlite/shop.py
+++ p_sqlite/shop.py
@@ -5,7 +5,8 @@ conn = sqlite3.connect('shop.db')
 curr = conn.cursor()
 
 
-
-
+# Create tables
+query = 'CREATE TABLE Orders(OrderNo TEXT PRIMARY KEY, Custid TEXT, ProdNo TEXT, Qty INTEGER, FOREIGN KEY(Custid) REFERENCES Customer(Custid), FOREIGN KEY(ProdNo) REFERENCES Product(ProdNo))'
+curr.execute(query)
 curr.close()
 conn.close()
